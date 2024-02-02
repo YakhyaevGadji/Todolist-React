b38dc4a381c2a30c61185b4d67bd05e55cbf1fb3
YakhyaevGadji <gadji180105@gmail.com>
2024-02-02 21:23:42 +0300

    reudx change

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Form from "../components/Form/Form";
 import ListItem from "../components/ListItem/ListItem";
-import { useSelector, useDispatch } from "react-redux";
-import { decrement, increment } from "../redux/slices/todoSlice";
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from '../redux/slices/todoSlice'
 
 const App = () => {
 	const [tasks, setTasks] = React.useState([
 		{id: +new Date(), text: 'hello world', done: false}
 	]);
-	const count = useSelector((state) => state.counter.count);
+	const [value, setValue] = React.useState('');
+	const count = useSelector((state) => state.counter.value);
   	const dispatch = useDispatch();
 
 	const addValue = (event) => {
@@ -17,6 +18,8 @@ const App = () => {
 		if(value.trim() !== '') {
 			setTasks([...tasks, {id: +new Date(), text: value, done: false}]);
 		}
+
+		setValue('')
 	};
 
 	const removeTask = (id) => {
@@ -40,26 +43,13 @@ const App = () => {
 
     return (
         <div className="wrapper-card">
-			<button
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          Increment
-        </button>
-        <span>{count}</span>
-        <button
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          Decrement
-        </button>
-			{/* <Form value={value} setValue={setValue} addValue={addValue}/>
+			<Form value={value} setValue={setValue} addValue={addValue}/>
 
 			<ul className="todo-list">
 				{tasks.map((task) => {
 					return <ListItem key={task.id} task={task} removeTask={removeTask} ahmadDone={ahmadDone}/>
 				})}
-			</ul> */}
+			</ul>
 		</div>
     );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,5 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './slices/todoSlice'
 
 export const store = configureStore({
-  reducer: counterReducer,
+  reducer: {
+    counter: counterReducer
+  },
 })
